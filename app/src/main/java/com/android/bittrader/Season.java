423c3245d01f1095c7ed663599075a053a0bbc88
Jason Trieu <j2trieu@ucsd.edu>
2017-09-01 17:04:57 -0700

    added season

--- app/src/main/java/com/android/bittrader/Season.java
+++ app/src/main/java/com/android/bittrader/Season.java
@@ -1,12 +1,14 @@
 package com.android.bittrader;
 
+import java.util.Random;
+
 /**
  * Created by Jason on 9/1/2017.
  */
 
 public class Season {
-    private int events[];
-    private int days[];
+    private int[] events;
+    private int[] days;
     private int season;
 
     /*
@@ -15,8 +17,38 @@ public class Season {
         season and add 1 to get the next season
      */
     public Season(int season){
-
+        this.season = season;
+        this.events = this.initializeEvents();
+        this.days = this.initializeDays(events);
     }
 
+    /*
+        @params: seed
+     */
+    private int[] initializeEvents(){
+        int numOfEvents = (int)(System.currentTimeMillis() % 5 + 1);
+        return new int[numOfEvents];
+    }
+
+    private int[] initializeDays(int[] events){
+        int[] days = new int[29 + (int)(System.currentTimeMillis() % 3)];
+        for(int i : events){
+            Random rand = new Random();
+            int dayOfEvent = rand.nextInt(days.length);
+            days[dayOfEvent] = 1;
+        }
+        return days;
+    }
+
+    @Override
+    public String toString(){
+        switch(season) {
+            case 0: return "Spring";
+            case 1: return "Summer";
+            case 2: return "Fall";
+            case 3: return "Winter";
+            default: return "Error";
+        }
+    }
 
 }
